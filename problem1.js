08525fffd73f60cd51ba52e94262ca3579ad6e44
Oli Nelson <olivernelson@icloud.com>
2020-04-06 13:16:04 +1000

    first commit

---
+++ problem1.js
@@ -0,0 +1,9 @@
+// Format: Row, Col
+
+// forbidden columns: 1 & 10
+
+function howManyFamilies (rows = 5, reservedSeats) {
+  console.log(reservedSeats)
+}
+
+howManyFamilies(3, [[2, 1], [1, 8], [2, 6]]) // 4
