efa76bd21b989d6dbf3d96506da9d4bc6ded7ffc
Oli Nelson <olivernelson@icloud.com>
2020-04-06 14:03:04 +1000

    finished problem1 simple solution

--- problem1.js
+++ problem1.js
@@ -1,42 +1,37 @@
-// Format: Row, Col
-
-// forbidden columns: 1 & 10
-
 function isSeatBooked (seat, reservedSeats) {
-  console.log('is this iseat booked', { seat })
   for (const r of reservedSeats) {
     if (seat[0] === r[0] && seat[1] === r[1]) {
-      console.log({ seat }, 'is booked')
       return true
     }
   }
   return false
 }
 
-function howManyFamilies (rows = 5, reservedSeats) {
-//   console.log(reservedSeats)
+function isThereRoomForAFamily (potentialSeats, reservedSeats) {
+  for (const s of potentialSeats) {
+    if (isSeatBooked(s, reservedSeats) === true) return false
+  }
+  return true
+}
 
+function howManyFamilies (rows = 5, reservedSeats) {
   let numberOfFamilies = 0
 
   for (let row = 1; row <= rows; row++) {
-    console.log({ row })
-    for (let seat = 1; seat <= 10; seat++) {
-    //   if (seat % 2 === 0) {
-      if (
-        isSeatBooked([row, seat], reservedSeats) &&
-        isSeatBooked([row, seat + 1], reservedSeats) &&
-        isSeatBooked([row, seat + 2], reservedSeats) &&
-        isSeatBooked([row, seat + 3], reservedSeats)
-      )
-      {
-        console.log('in condition')
-        numberOfFamilies++
+    for (let seat = 1; seat <= 6; seat++) {
+      if (seat % 2 === 0) {
+        const potentialSeats = [
+          [row, seat],
+          [row, seat + 1],
+          [row, seat + 2],
+          [row, seat + 3]
+        ]
+
+        if (isThereRoomForAFamily(potentialSeats, reservedSeats)) {
+          numberOfFamilies++
+          seat += 3
+        }
       }
-      // console.log('is seat booked', isSeatBooked([row, seat], reservedSeats))
-      // if (seat)
-      // const familySpace = 0
-      // console.log({ seat })
-    //   }
     }
   }
 
@@ -44,4 +39,6 @@ function howManyFamilies (rows = 5, reservedSeats) {
   return numberOfFamilies
 }
 
-howManyFamilies(3, [[2, 1], [1, 8], [2, 6]]) // 4
+howManyFamilies(3, [[1, 2], [1, 3], [1, 8], [2, 6], [3, 1], [3, 10]]) // 4
+howManyFamilies(2, [[2, 1], [1, 8], [2, 6]]) // 2
+howManyFamilies(4, [[4, 3], [1, 4], [4, 6], [1, 7]]) // 4
